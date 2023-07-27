a4d40086926f53fb85fe5be1979ac522e6d727bb
Sergio <sergiomolinaflores1@gmail.com>
2023-07-26 22:19:34 -0400

    aplicacion guardando en la base da datos

--- app/src/main/java/cl/samf/individual1_mod6/AgregarFragment.kt
+++ app/src/main/java/cl/samf/individual1_mod6/AgregarFragment.kt
@@ -5,55 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cl.samf.individual1_mod6.databinding.FragmentAgregarBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AgregarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AgregarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var binding: FragmentAgregarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar, container, false)
+        binding = FragmentAgregarBinding.inflate(layoutInflater,container,false)
+
+        initListener()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AgregarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AgregarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initListener() {
+    binding.buttonAgregar.setOnClickListener{
+        val texto = binding.editTextIngresoTarea.text.toString()
+        guardarTarea(texto)
     }
+    }
+
+    private fun guardarTarea(texto:String) {
+        val dao= TareaBaseDatos.getDatabase(requireContext()).getTaskDao()
+        val tarea = Tarea(texto, "10-05-2023")
+        GlobalScope.launch { dao.insertarTarea(tarea) }
+
+    }
+
+
 }
