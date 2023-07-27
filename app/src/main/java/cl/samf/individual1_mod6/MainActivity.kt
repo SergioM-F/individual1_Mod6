9f0d0f311abe37a4984cc9be73a2d00911d7899e
Sergio <sergiomolinaflores1@gmail.com>
2023-07-26 20:21:52 -0400

    Implementado Navigation en Activity main

--- app/src/main/java/cl/samf/individual1_mod6/MainActivity.kt
+++ app/src/main/java/cl/samf/individual1_mod6/MainActivity.kt
@@ -3,6 +3,20 @@ package cl.samf.individual1_mod6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+[ ] Navigation
+[ ] Framento Agregar
+[ ] Crear Interfaz
+[ ] Room
+    [ ] _Dao
+    [ ] _Base Datos
+    [ ] _Entity
+[ ] Fragmento listado
+[ ] RecyclerView
+
+*/
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
