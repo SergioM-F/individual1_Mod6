16e129a2b235e2ee08259ce8d8b9e31131da395a
Sergio <sergiomolinaflores1@gmail.com>
2023-07-28 19:32:22 -0400

    Invidual 3 Mod 6 Ok

--- app/src/main/java/cl/samf/individual1_mod6/AgregarFragment.kt
+++ app/src/main/java/cl/samf/individual1_mod6/AgregarFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import cl.samf.individual1_mod6.databinding.FragmentAgregarBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -15,7 +16,7 @@ import kotlinx.coroutines.launch
 class AgregarFragment : Fragment() {
 
     lateinit var binding: FragmentAgregarBinding
-    lateinit var repositorio: Repositorio
+    private val tareaViewModel : TareaViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +29,13 @@ class AgregarFragment : Fragment() {
     ): View? {
         binding = FragmentAgregarBinding.inflate(layoutInflater,container,false)
 
-        initRepositorio()
+
         initListener()
         cargarTareas()
         return binding.root
     }
 
-    private fun initRepositorio() {
-        repositorio = Repositorio(TareaBaseDatos.getDatabase(requireContext()).getTaskDao())
-    }
+
 
     private fun initListener() {
     binding.buttonAgregar.setOnClickListener{
@@ -47,14 +46,14 @@ class AgregarFragment : Fragment() {
 
     private fun guardarTarea(texto:String) {
 
-        val tarea = Tarea(texto, "10-05-2023")
-        GlobalScope.launch { repositorio.insertTarea(tarea) }
+        val tarea = Tarea(texto,"")
+        tareaViewModel.insertarTareas(tarea)
 
     }
 
     private fun cargarTareas() {
 
-        repositorio.obtenerTarea().observe(requireActivity()){
+        tareaViewModel.obtenerTareas().observe(viewLifecycleOwner){
             val tareasTexto = it.joinToString("\n") { it.nombre + it.fecha } //se puede concatenar con + para agregar datos que uno quiera sumar o solo el it para agregar todo
             binding.textViewMostrar.text = tareasTexto
 
